04d9eac2d78232b94c9d8e4642373fb2090642b0
Anna <filatovaa@yahoo.com>
2020-09-28 23:32:49 -0700

    header layed out with basic drop down functionality

--- js/site_functions.js
+++ js/site_functions.js
@@ -1,3 +1,26 @@
 $(function(){
+    $(".button").click(function() {
+        let val = $(this).attr('id');
+        if (val == 0) {
+            $("ul").hide();
+            $(this).attr('id', '1');
+        } else {
+            $("ul").show();
+            $(this).attr('id', '0');
+        }
+
+    });
+
+    //Mouse click on setting button and ul list
+    $("ul, .button").mouseup(function() {
+        return false;
+    });
+
+    //Document Click
+    $(document).mouseup(function() {
+        $("ul").hide();
+        $(".button").attr('id', '0');
+    });
+    
   
 });
