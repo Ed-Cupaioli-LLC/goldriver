371fb2226a61b279e586cfdfd27e18b8e76dd924
Anna Filatova <annafilatova@Annas-MBP.wavecable.com>
2020-10-14 12:38:59 -0700

    you tube api

--- js/insta.js
+++ js/insta.js
@@ -21,6 +21,11 @@ $(function(){
           });  
 
     } 
+
+
+
+
+    
 })
 
 
@@ -79,3 +84,44 @@ $(function(){
 //   }
 // })()
 
+
+var player;
+function onYouTubePlayerAPIReady() {
+player = new YT.Player('ytplayer', {
+width: '100%',
+height: '100%',
+videoId: '3rnt4hv_Qv4',
+playerVars: {
+'autoplay': 1,
+'showinfo': 0,
+'autohide': 1,
+'loop': 1,
+'controls': 0,
+'modestbranding': 1,
+'vq': 'hd1080'
+},
+events: {
+'onReady': onPlayerReady,
+'onStateChange': onPlayerStateChange
+}
+});
+}
+
+// 4. The API will call this function when the video player is ready.
+function onPlayerReady(event) {
+event.target.playVideo();
+player.mute(); // comment out if you don't want the auto played video muted
+}
+
+// 5. The API calls this function when the player's state changes.
+// The function indicates that when playing a video (state=1),
+// the player should play for six seconds and then stop.
+function onPlayerStateChange(event) {
+if (event.data == YT.PlayerState.ENDED) {
+player.seekTo(0);
+player.playVideo();
+}
+}
+function stopVideo() {
+player.stopVideo();
+}
