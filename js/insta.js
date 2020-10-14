054bdd7a7a15cb7c1ae93d5edfbb4e2229b9a79b
Anna Filatova <annafilatova@Annas-MBP.wavecable.com>
2020-10-14 00:40:18 -0700

    updates to insta portion

--- js/insta.js
+++ js/insta.js
@@ -1,55 +1,28 @@
 //--------------------------------API call from Instagram in case the code below will fail------------------------
 
-// $(function(){
- 
-//     var arr = ['p/B_frooMAdGx','p/CFz9bHXHIxw','p/CFsN2mnnfcZ','p/CBOQqJmnFv8','p/CDkCs1xohWr','p/CByslopA2kt','p/CCpJG09Jv2W', 'p/CARNYS2JOgJ',]
-//     for(var i=0; i<arr.length; i++){
-//             $.ajax({
-//             // url: 'https://graph.facebook.com/v8.0/instagram_oembed?url=https://www.instagram.com/'+arr[i]+'&maxwidth=320&html&fields=thumbnail_url%2Cauthor_name%2Cprovider_name%2Cprovider_url%_html&access_token=401728117501247|5b862f9602225228ae6addaf22183941',
-//             url: 'https://graph.facebook.com/v8.0/instagram_oembed?url=https://www.instagram.com/'+arr[i]+'/&maxwidth=320&access_token=401728117501247|5b862f9602225228ae6addaf22183941',
-//             type: 'GET',
-//             dataType: 'json',
-//             success: function(data) {
-//               var $img = $("<img>", {src: data.thumbnail_url, "class": "image"});
-//               var $div = $('<div>', {'class':'hidden'}).append(data.html)
-//               console.log(data)
-//               $('.insta-image').append($img) 
-//               $('.insta-image').append($div)
-             
-//               instgrm.Embeds.process()
-//             }
-//           });  
-
-//     } 
-// })
-
 $(function(){
  
-  var arr = ['p/B_frooMAdGx','p/CFz9bHXHIxw','p/CFsN2mnnfcZ','p/CBOQqJmnFv8','p/CDkCs1xohWr','p/CByslopA2kt','p/CCpJG09Jv2W', 'p/CARNYS2JOgJ',]
-  for(var i=0; i<arr.length; i++){
-          $.ajax({
-          // url: 'https://graph.facebook.com/v8.0/instagram_oembed?url=https://www.instagram.com/'+arr[i]+'&maxwidth=320&html&fields=thumbnail_url%2Cauthor_name%2Cprovider_name%2Cprovider_url%_html&access_token=401728117501247|5b862f9602225228ae6addaf22183941',
-          url: 'https://graph.facebook.com/v8.0/instagram_oembed?url=https://www.instagram.com/'+arr[i]+'/&maxwidth=320&access_token=401728117501247|5b862f9602225228ae6addaf22183941',
-          type: 'GET',
-          dataType: 'json',
-          success: function(data) {
-            var $img = $("<img>", {'src': data.thumbnail_url, "class": "image"})
-            var $div = $('<div>', {'class':'hidden'}).append(data.html)
-            console.log(data)
-         
-            $('.insta-data').append($div)
-          //   $img.appendTo('a') 
-          $img.appendTo('.container')
-            
-         
-            
-           
-            instgrm.Embeds.process()
-          }
-        });  
+    var arr = ['p/B_frooMAdGx','p/CFz9bHXHIxw','p/CFsN2mnnfcZ','p/CBOQqJmnFv8','p/CDkCs1xohWr','p/CByslopA2kt','p/CCpJG09Jv2W', 'p/CARNYS2JOgJ',]
+    for(var i=0; i<arr.length; i++){
+            $.ajax({
+            // url: 'https://graph.facebook.com/v8.0/instagram_oembed?url=https://www.instagram.com/'+arr[i]+'&maxwidth=320&html&fields=thumbnail_url%2Cauthor_name%2Cprovider_name%2Cprovider_url%_html&access_token=401728117501247|5b862f9602225228ae6addaf22183941',
+            url: 'https://graph.facebook.com/v8.0/instagram_oembed?url=https://www.instagram.com/'+arr[i]+'/&maxwidth=320&access_token=401728117501247|5b862f9602225228ae6addaf22183941',
+            type: 'GET',
+            dataType: 'json',
+            success: function(data) {
+              var $img = $("<img>", {src: data.thumbnail_url, "class": "image"});
+              var $div = $('<div>', {'class':'hidden'}).append(data.html)
+              console.log(data)
+              $('.insta-image').append($img) 
+              $('.insta-image').append($div)
+             
+              instgrm.Embeds.process()
+            }
+          });  
 
-  } 
+    } 
 })
+
 
 
 //--------------------------------Graph QL solution without API call ----------------------------->
